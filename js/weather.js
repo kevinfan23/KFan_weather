f1c4ea997608524da7d8c7fb43c48122b9242015
kevinfan23 <kevinfan23@ucla.edu>
2016-02-25 01:28:57 -0800

    Version 1.0

--- js/weather.js
+++ js/weather.js
@@ -1,38 +1,39 @@
 if ("geolocation" in navigator) {
-	navigator.geolocation.getCurrentPosition(function(position) {
-		getWeather(position.coords.latitude + ',' + position.coords.longitude);
-	});
+    navigator.geolocation.getCurrentPosition(function(position) {
+        getWeather(position.coords.latitude + ',' + position.coords.longitude);
+    });
 }
 else {
-	getWeather("Los Angeles, US");
+    getWeather("Los Angeles, CA");
 }
 
-$(document).ready(function() {
-	setInterval(getWeather, 10000);
+$(document).ready(function() {  
+  setInterval(getWeather, 600000); //Update the weather every 10 minutes.
 });
 
-function getWather(location) {
-	$.simpleWeather({
-		location: 'Boston, MA',
-		woeid: '2357536',
-		unit: 'c',
-		success: function(weather) {
-			var city = weather.city;
-			var temp = weather.temp + '&deg;';
-			var wcode = '<img class="weathericon" src="images/weathericons/' + weather.code + '.svg">';
-			var wind = '<p>' + weather.wind.speed + '</p><p>' + weather.units.speed + '</p>';
-			var humidity = weather.humidity + ' %';
-			
-			$(".location").text(city);
-			$(".temperature").html(temp);
-			$(".climate_bg").html(wcode);
-			$(".windspeed").html(wind);
-			$(".humidity").text(humidity);
-		},
-		
-		error : function(error) {
-			$(".error").html('<p>' + error + '</p>');
-		}
-	});
-}
 
+function getWeather(location) {
+	
+	$.simpleWeather({
+    location: location,
+    woeid: '',
+    unit: 'c',
+    success: function(weather) {
+	    var city = weather.city;
+        var temp = weather.temp + '&deg;';
+        var wcode = '<img class="weathericon" src="images/weathericons/' + weather.code + '.svg">';
+        var wind = '<p>' + weather.wind.speed + '</p><p>' + weather.units.speed + '</p>';
+        var humidity = weather.humidity + ' %';
+  
+		$(".location").text(city);
+        $(".temperature").html(temp);
+        $(".climate_bg").html(wcode);
+        $(".windspeed").html(wind);
+        $(".humidity").text(humidity);
+    },
+    
+    error : function(error) {
+            $(".error").html('<p>' + error + '</p>');
+    }
+  });
+}
